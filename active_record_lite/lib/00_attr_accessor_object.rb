137e89ede33c4d2f0d86fc7977ada5d2e003d23e
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-14 16:18:03 -0500

    Complete 00_attr_accessor_object

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do 
+        self.instance_variable_get("@#{name}")
+      end
+    end
+    names.each do |name|
+      define_method("#{name}=") do |value|
+        self.instance_variable_set("@#{name}".to_sym, value)
+      end
+    end
   end
 end
